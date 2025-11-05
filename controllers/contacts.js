0b92bae134f778c4bcb1a9a8fb336b521fa888d1
Daniel25TE <danielalejandrosud25@gmail.com>
2025-11-05 15:33:54 -0700

    Contacts project part 2, Step 1

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -19,4 +19,51 @@ const getSingle = async (req, res) => {
     });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+    if (response.acknowledged) {
+        res.status(201).json({
+          message: 'Contact created successfully',
+          contactId: response.insertedId
+        });
+
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while inserting the user.')
+    }
+};
+
+const updateContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId }, contact);
+    if (response.modifiedCount > 0) {
+        res.status(200).json({ message: 'Contact updated successfully' });
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.')
+    }
+};
+
+const deleteContact = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId });
+    if (response.deletedCount > 0) {
+        res.status(200).json({ message: 'Contact deleted successfully' });
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the user.')
+    }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
